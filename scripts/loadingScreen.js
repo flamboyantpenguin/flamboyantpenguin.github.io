f8eaa5b018f148391c42c1a5cde162211611faa4
flamboyantpenguin <shapedana@gmail.com>
2025-08-14 10:11:22 +0530

    Added Experimental Loading Buffer

---
+++ scripts/loadingScreen.js
@@ -0,0 +1,9 @@
+window.addEventListener('load', function() {
+
+    var title_card_border = document.getElementById("titleCardWrapperMain")
+    title_card_border.className += " ready";
+    
+    var title_card = document.getElementById("titleCardMain")
+    title_card.className += " ready";
+
+})
