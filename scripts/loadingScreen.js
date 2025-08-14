46c114c547cfaea591ef252e7983d6bb8c4e1e7c
flamboyantpenguin <shapedana@gmail.com>
2025-08-14 15:26:19 +0530

    Fixed Responsive UI for Phones

--- scripts/loadingScreen.js
+++ scripts/loadingScreen.js
@@ -1,7 +1,4 @@
 window.addEventListener('load', function() {
-
-    var title_card_border = document.getElementById("titleCardWrapperMain")
-    title_card_border.className += " ready";
     
     var title_card = document.getElementById("titleCardMain")
     title_card.className += " ready";
