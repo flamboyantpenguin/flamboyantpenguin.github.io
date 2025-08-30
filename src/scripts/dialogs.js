4b28b0e20da469ef86efad4e9289338250af05b3
flamboyantpenguin <shapedana@gmail.com>
2025-08-30 22:27:33 +0530

    Fixed Dialg bug

--- src/scripts/dialogs.js
+++ src/scripts/dialogs.js
@@ -4,9 +4,6 @@ var emailDialog = document.getElementById("emailDialog");
 var chatButton = document.getElementById("chatButton");
 var chatDialog = document.getElementById("chatDialog");
 
-var weatherButton = document.getElementById("weatherButton");
-var weatherDialog = document.getElementById("weatherDialog");
-
 emailButton.addEventListener('click', async () => {
   emailDialog.classList.toggle("show")
 });
@@ -15,11 +12,8 @@ chatButton.addEventListener('click', async () => {
   chatDialog.classList.toggle("show")
 });
 
-weatherButton.addEventListener('click', async () => {
-  weatherDialog.classList.toggle("show")
-});
-
 document.addEventListener('click', (event) => {
+  
   const isClickOutsideEmailDialog = !emailButton.contains(event.target) && !emailDialog.contains(event.target);
   if (isClickOutsideEmailDialog) {
     emailDialog.classList.remove("show");
@@ -30,8 +24,4 @@ document.addEventListener('click', (event) => {
     chatDialog.classList.remove("show");
   }
 
-  const isClickOutsideWeatherDialog = !weatherButton.contains(event.target) && !weatherDialog.contains(event.target);
-  if (isClickOutsideWeatherDialog) {
-    weatherDialog.classList.remove("show");
-  }
 });
