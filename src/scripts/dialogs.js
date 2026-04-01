f9769587802542a5b681bd823944720c8625a1d7
flamboyantpenguin <mail@flamboyantpenguin.in>
2026-04-02 01:03:37 +0530

    feat: add pgp keys
    Signed-off-by: flamboyantpenguin <mail@flamboyantpenguin.in>

--- src/scripts/dialogs.js
+++ src/scripts/dialogs.js
@@ -4,12 +4,19 @@ var emailDialog = document.getElementById("emailDialog");
 var chatButton = document.getElementById("chatButton");
 var chatDialog = document.getElementById("chatDialog");
 
+var pgpButton = document.getElementById("pgpButton");
+var pgpDialog = document.getElementById("pgpDialog");
+
 emailButton.addEventListener('click', async () => {
   emailDialog.classList.toggle("show")
 });
 
 chatButton.addEventListener('click', async () => {
   chatDialog.classList.toggle("show")
+});
+
+pgpButton.addEventListener('click', async () => {
+  pgpDialog.classList.toggle("show")
 });
 
 document.addEventListener('click', (event) => {
@@ -22,6 +29,11 @@ document.addEventListener('click', (event) => {
   const isClickOutsideChatDialog = !chatButton.contains(event.target) && !chatDialog.contains(event.target);
   if (isClickOutsideChatDialog) {
     chatDialog.classList.remove("show");
+  }
+
+  const isClickOutsidePGPDialog = !pgpButton.contains(event.target) && !pgpDialog.contains(event.target);
+  if (isClickOutsidePGPDialog) {
+    pgpDialog.classList.remove("show");
   }
 
 });
